56409c2f2becb654e6196e8b49a1b1b92fd7b78e
Suhani <suhaninegi124@gmail.com>
2026-05-06 14:58:09 +0530

    origin of cors changed to *

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ import protect from './middleware/authMiddleware.js';
 
 const app=express();
 
-app.use(cors({origin: "http://localhost:5173" , credentials:true ,}));  //allows cookie and authentication headers to be sent
+app.use(cors({origin: "*" , credentials:true ,}));  //allows cookie and authentication headers to be sent
 app.use(cookieParser()) //Because we are using cookie
 app.use(express.json()); //parses form data (like HTML form submissions) into a JavaScript object.
 app.use(express.urlencoded({extended:false})); //Parses JSON request body into JavaScript object.
