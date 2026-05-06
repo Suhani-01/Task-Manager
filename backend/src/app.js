8d6de7f00c8de733447efb26293180c5a1b88a3f
Suhani <suhaninegi124@gmail.com>
2026-05-06 15:02:55 +0530

    origin changed to the original frontend url

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ import protect from './middleware/authMiddleware.js';
 
 const app=express();
 
-app.use(cors({origin: "*" , credentials:true ,}));  //allows cookie and authentication headers to be sent
+app.use(cors({origin: "https://task-manager-ebon-nine-66.vercel.app" , credentials:true ,}));  //allows cookie and authentication headers to be sent
 app.use(cookieParser()) //Because we are using cookie
 app.use(express.json()); //parses form data (like HTML form submissions) into a JavaScript object.
 app.use(express.urlencoded({extended:false})); //Parses JSON request body into JavaScript object.
